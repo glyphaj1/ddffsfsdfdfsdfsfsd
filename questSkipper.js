f8bb3af6a3f9d52b023dcada4be90fd18dc68dfd
cmd <148266754+glyphaj1@users.noreply.github.com>
2026-01-07 20:41:29 +0000

    Add Discord Quest Skipper script
    This script sets the playback rate of all video elements to 16x for faster viewing.

---
+++ questSkipper.js
@@ -0,0 +1,7 @@
+// Discord Quest Skipper ( Grind ORBS! )
+
+// Go to "https://discord.com/quest-home"
+// Click Ctrl + Shift + I
+// Go to console
+// Copy and paste all of this and then watch the video!
+document.querySelectorAll("video").forEach(v => v.playbackRate = 16);
